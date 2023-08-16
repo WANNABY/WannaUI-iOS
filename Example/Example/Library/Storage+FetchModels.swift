0285dcf94268c7cd96b291194fa39a95d4161552
Denis Kapusta <dkapusta89@gmail.com>
2023-08-16 11:50:09 +0300

    [WK-8153] Update to WannaSDK 6.1.0 #4

--- Example/Example/Library/Storage+FetchModels.swift
+++ Example/Example/Library/Storage+FetchModels.swift
@@ -5,10 +5,14 @@ import Foundation
 import WsneakersUISDK
 
 public extension WsneakersUISDKRenderModelStorage {
+    enum Error: Swift.Error {
+        case unexpectedNoError
+    }
+
     func fetchModels(completion: @escaping (Result<[WsneakersUISDKModelInfo], Swift.Error>) -> Void) {
         loadModels { models, error in
             guard let models = models else {
-                completion(.failure(error ?? Error.unexpected))
+                completion(.failure(error ?? Error.unexpectedNoError))
                 return
             }
             completion(.success(models))
